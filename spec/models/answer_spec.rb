ddc7a8a191b588bb93349888012fc66be4889d36
aaz <ilk-ui@yandex.ru>
2022-07-08 22:43:30 +0300

    Created Question and Answer models and specs for them

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  describe 'associations' do
+    it { should belong_to(:question) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body }
+    it { should validate_presence_of :question_id }
+  end
+
+  it { should have_db_index :question_id }
+end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
-
-  it 'validates presence of title' do
-    expect(Question.new(body: '123')).to_not be_valid
+  describe 'associations' do
+    it { should have_many(:answers) }
   end
 
-  it 'validates presence of body' do
-    expect(Question.new(title: 'question_title')).to_not be_valid
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
   end
 end
