8748b414dbd81afad6ef91ac5cd9396aee5c363f
aaz <ilk-ui@yandex.ru>
2022-07-11 17:16:19 +0300

    Remove before_action for load_question

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -9,6 +9,4 @@ RSpec.describe Answer, type: :model do
     it { should validate_presence_of :body }
     it { should validate_presence_of :question_id }
   end
-
-  it { should have_db_index :question_id }
 end
