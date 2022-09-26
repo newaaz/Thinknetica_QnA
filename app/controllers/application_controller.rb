95ccf1d392dbbe25b7a343e1ca64c73be39b53b7
aaz <ilk-ui@yandex.ru>
2022-09-26 12:42:11 +0300

    add authorization with cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :set_gon_current_user 
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
+  #check_authorization
+  #skip_authorization
+
   private 
 
   def set_gon_current_user
